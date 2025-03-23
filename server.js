30fc616b706abe7b2ea29ef2cc89d50fea93d88d
Mickael Joseph <mickaelandriana06@gmail.com>
2025-03-24 00:29:52 +0300

    remove all useless code

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const express  = require('express')
-const Possession = require('./src/models/Possession')
-const Batiment = require('./src/models/Batiment')
-const sequelize = require('./src/configs/sequelize')
-// const Fixed = require('./src/models/Fixed')
+
 
 
 
 const app = express()
-
-const q  = async()=>{
-    if(sequelize) {
-        Batiment.sync({force:true})
-        // Batiment.sync({alter:true})
-    }
-}
-// q()
 
 
 const PORT = process.env.SERVER_PORT ?? 3000 
