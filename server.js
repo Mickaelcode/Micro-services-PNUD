2ffd9c0660af0fa98e10660ada195ed76c59f98f
Mickael Joseph <mickaelandriana06@gmail.com>
2025-03-23 22:46:38 +0300

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express  = require('express')
-require("dotenv").config();
-
+const sequelize = require('./src/configs/sequelize')
 const app = express()
 
 
--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -1,5 +1,25 @@
-const Sequelize = require('sequelize')
-require('dotenv').config()
+const Sequelize = require("sequelize");
+const config = require("./config.js");
 
+const env = process.env.NODE_ENV || "development";
+const dbConfig = config[env];
 
-const sequelize = new Sequelize(process.env.DATA)
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+  }
+);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connexion à la base de données réussie");
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à la base de données :", err);
+  });
+
+module.exports = sequelize;
