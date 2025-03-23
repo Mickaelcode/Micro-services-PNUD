47844aa9e699642066cb3e86e411bf047fc6e9a8
Mickael Joseph <mickaelandriana06@gmail.com>
2025-03-24 01:42:16 +0300

    add terrain route to main route

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
 const possessionRoute = require('./PossessionRoute')
 const batimentRoute = require('./BatimentRoute')
+const terrainRoute = require('./TerrainRoute')
 
 const routes = Router()
 
 routes.use('/possession',possessionRoute)
 routes.use('/batiment',batimentRoute)
+routes.use('/terrain',terrainRoute)
 
 module.exports = routes
